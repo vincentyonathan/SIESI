5145a24000a0beb5d17304a9212b496c00a5aa27
vincentyonathan <vincentyonathan24@gmail.com>
2021-12-04 11:33:11 +0700

    some

--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Selamat Datang di SIEITS</title>
+    <title>Selamat Datang ke SIEITS</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="shortcut icon" href="/images/its.ico" />
     <style>

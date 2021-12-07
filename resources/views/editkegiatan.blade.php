1468da654a41e8c1fe8251d6daded85497e48b0f
Bayu Adjie Sidharta <31591861+ADryInkCartridge@users.noreply.github.com>
2021-12-07 23:21:45 +0700

    Merge pull

--- resources/views/editkegiatan.blade.php
+++ resources/views/editkegiatan.blade.php
@@ -230,9 +230,9 @@
                                     </div>
                                     <div class="h-20">
                                         <div class="flex flex-row pl-4">
-                                            <button class="add flex flex-row">
+                                            <button onclick="openmodal()" class="add flex flex-row">
                                                 <img src="/images/plus.png" alt="" class="h-8">
-                                                <h1 class="pt-1 pl-2">Tambah Kegiatan</h1>
+                                                <h1 class="pt-1 pl-2">Tambah Capaian</h1>
                                             </button>
                                         </div>
                                     </div>
@@ -261,6 +261,34 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div id="modaltambahcapaian" style="display: none;" class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div class="sm:flex sm:items-start">
+            <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <label for="name" class="pt-4 text-base font-medium">Nama Capaian</label>
+                <input name="name" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nama Capaian">                      
+                <label for="message" class="pt-4 text-base font-medium">Deskripsi Capaian</label>
+                <textarea id="message" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Deskripsi Capaian"></textarea>
+                <label for="bobot" class="pt-4 text-base font-medium">Bobot</label>
+                <input name="bobot" type="number" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-20 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Bobot">                      
+            </div>
+            </div>
+        </div>
+        <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
+            Submit
+            </button>
+            <button onclick="closemodal()" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+            Cancel
+            </button>
+        </div>
+        </div>
+    </div>
     </div>
 
     <script>
@@ -298,58 +326,18 @@
         }
       </script> 
       <script>
-        // Get the modal
-        var modal = document.getElementById("myModal");
-
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks the button, open the modal 
-        btn.onclick = function() {
-        modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-        modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-        }
+          function openmodal()
+          {
+                let x = document.getElementById("modaltambahcapaian")
+                x.style.display = "block"
+          }
       </script>
       <script>
-        // Get the modal
-        var modal = document.getElementById("myModal2");
-
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn2");
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks the button, open the modal 
-        btn.onclick = function() {
-        modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-        modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-        }
+          function closemodal()
+          {
+                let x = document.getElementById("modaltambahcapaian")
+                x.style.display = "none"
+          }
       </script>
       <script>
         window.addEventListener('DOMContentLoaded', () => {

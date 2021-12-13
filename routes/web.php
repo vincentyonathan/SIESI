b1dd42fa2c7c9d48ac504143a7c04fe2c32bbd78
Ryan Garnet Andrianto <ryangarnetandrianto@gmail.com>
2021-12-13 12:22:22 +0700

    Tambah middleware auth
    Untuk mencegah user yang belum login mengakses halaman kegiatan

--- routes/web.php
+++ routes/web.php
@@ -19,30 +19,33 @@ use App\Http\Controllers\NilaiController;
 
 // USER
 
-Route::get('/',[KegiatanController::class,'mainpage'])->name('mainpage');
 
 Route::GET('/login',[UserController::class,'login_page'])->name('login');
 Route::POST('/login/post',[UserController::class,'login'])->name('login.post');
 
-Route::post('/logout',[UserController::class,'logout'])->name('logout');
-// KEGIATAN
-
-Route::get('/addkegiatan/',[KegiatanController::class,'addkegiatanpage'])->name('addKegiatan');
-Route::POST('/addkegiatan/post',[KegiatanController::class,'addKegiatan'])->name('addKegiatan.post');
-
-Route::get('/editkegiatan/{id}',[KegiatanController::class,'editkegiatanpage'])->name('editkegiatan');
-Route::post('/editkegiatan/post',[KegiatanController::class,'editKegiatan'])->name('editkegiatan.post');
-
-Route::get('/deletekegiatan/{id}',[KegiatanController::class,'deleteKegiatan'])->name('deletekegiatan');
-Route::get('/gantistatus/{id}',[KegiatanController::class,'gantiStatus'])->name('gantistatus');
-Route::get('/hasilsurvey/{id}',[KegiatanController::class,'hasilsurveypage'])->name('hasilsurvey');
-
-// CAPAIAN
-
-Route::post('/addcapaian/post',[PencapaianController::class,'addCapaian'])->name('addcapaian.post');
-Route::get('/deletecapaian/{id}/{keg}',[PencapaianController::class,'deleteCapaian'])->name('deletecapaian');
-
-// NILAI / SURVEY
-
-Route::get('/isisurvey/{id}',[NilaiController::class,'isisurveypage'])->name('isisurvey');
-Route::post('/isisurvey/post',[NilaiController::class,'addNilai'])->name('isisurvey.post');
+Route::middleware(['auth'])->group(function() {
+    Route::get('/',[KegiatanController::class,'mainpage'])->name('mainpage');
+    
+    Route::post('/logout',[UserController::class,'logout'])->name('logout');
+    // KEGIATAN
+    
+    Route::get('/addkegiatan/',[KegiatanController::class,'addkegiatanpage'])->name('addKegiatan');
+    Route::POST('/addkegiatan/post',[KegiatanController::class,'addKegiatan'])->name('addKegiatan.post');
+    
+    Route::get('/editkegiatan/{id}',[KegiatanController::class,'editkegiatanpage'])->name('editkegiatan');
+    Route::post('/editkegiatan/post',[KegiatanController::class,'editKegiatan'])->name('editkegiatan.post');
+    
+    Route::get('/deletekegiatan/{id}',[KegiatanController::class,'deleteKegiatan'])->name('deletekegiatan');
+    Route::get('/gantistatus/{id}',[KegiatanController::class,'gantiStatus'])->name('gantistatus');
+    Route::get('/hasilsurvey/{id}',[KegiatanController::class,'hasilsurveypage'])->name('hasilsurvey');
+    
+    // CAPAIAN
+    
+    Route::post('/addcapaian/post',[PencapaianController::class,'addCapaian'])->name('addcapaian.post');
+    Route::get('/deletecapaian/{id}/{keg}',[PencapaianController::class,'deleteCapaian'])->name('deletecapaian');
+    
+    // NILAI / SURVEY
+    
+    Route::get('/isisurvey/{id}',[NilaiController::class,'isisurveypage'])->name('isisurvey');
+    Route::post('/isisurvey/post',[NilaiController::class,'addNilai'])->name('isisurvey.post');
+});
